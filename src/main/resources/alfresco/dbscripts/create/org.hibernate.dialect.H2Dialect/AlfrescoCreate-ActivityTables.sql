fd0aed772525d0c39b0b7411dd252e73bdda93ff
Carlo Sciolla <skuro@skuro.tk>
2013-03-26 17:07:18 +0100

    Sync blank lines for better changes detection

--- src/main/resources/alfresco/dbscripts/create/org.hibernate.dialect.H2Dialect/AlfrescoCreate-ActivityTables.sql
+++ src/main/resources/alfresco/dbscripts/create/org.hibernate.dialect.H2Dialect/AlfrescoCreate-ActivityTables.sql
@@ -1,18 +1,18 @@
 -- Copyright (c) 2011 Carlo Sciolla
 --
--- Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
--- and associated documentation files (the "Software"), to deal in the Software without restriction, 
--- including without limitation the rights to use, copy, modify, merge, publish, distribute, 
--- sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+-- and associated documentation files (the "Software"), to deal in the Software without restriction,
+-- including without limitation the rights to use, copy, modify, merge, publish, distribute,
+-- sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
 -- is furnished to do so, subject to the following conditions:
 --
--- The above copyright notice and this permission notice shall be included in all copies or 
+-- The above copyright notice and this permission notice shall be included in all copies or
 -- substantial portions of the Software.
 --
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
--- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
--- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
--- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+-- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+-- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --
 --
@@ -21,7 +21,6 @@
 -- Database:   PostgreSQL
 -- Since:      V3.0 Schema 126
 -- Author:     janv
-
 
 CREATE SEQUENCE alf_activity_feed_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE alf_activity_feed
@@ -53,7 +52,7 @@ CREATE TABLE alf_activity_feed_control
     site_network VARCHAR(255),
     app_tool VARCHAR(36),
     last_modified TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)    
+    PRIMARY KEY (id)
 );
 CREATE INDEX feedctrl_feeduserid_idx ON alf_activity_feed_control (feed_user_id);
 
